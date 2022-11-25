4c0800c5dbba51ed9a6d21ff6a71f9b0f3874323
duechayapolgmit <duechayapolsbs@gmail.com>
2022-11-25 12:15:18 +0000

    more functionality

--- lab-three/src/components/fcreate.js
+++ lab-three/src/components/fcreate.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 // functional component -- uses functions instead of classes
 export function FCreate(props){
@@ -7,10 +9,34 @@ export function FCreate(props){
     const [cover, setCover] = useState('');
     const [author, setAuthor] = useState('');
 
+    // navigation function - enabling navigation
+    const navigate = useNavigate(); 
+
+    // same functionality as componentDidMount in class components
+    useEffect(() => {   
+        setTitle('');
+        setCover('');
+        setAuthor('');
+    },[]); // The [] prevents this to be called multiple times
+
     // handle submission of the form -- no need for this.state as it already handles it via useState() above
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(`${title} ${cover} ${author}`) // Debug
+
+        // newBook object - to be put into database via axios
+        const newBook = {
+            title:title,
+            cover:cover,
+            author:author
+        }
+
+        // AXIOS handling for POST request
+        axios.post('http://localhost:4000/api/books', newBook)
+          .then((res)=>{
+            navigate('/read') // navigate to read page
+          }) 
+          .catch();
     }
 
     // unlike class component, returns directly without .render method
